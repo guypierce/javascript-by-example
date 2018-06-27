7f57aecde23a10f290b20de78c47380228fbd936
guypierce <14934009+guypierce@users.noreply.github.com>
2018-06-27 09:52:45 -0600

    Create ternary-operator-race-registration.js

---
+++ ternary-operator-race-registration.js
@@ -0,0 +1,13 @@
+// Ternary Control Flow
+let raceNumber = Math.floor(Math.random() * 1000),
+registeredEarly = true;
+const runnerAge = 9; 
+
+runnerAge >= 18 && registeredEarly ?
+  console.log(`${raceNumber + 1000 }
+The race will begin at 9:30am`)
+	: runnerAge >= 18 && !registeredEarly ?
+    console.log(`${raceNumber}
+The race will begin at 11:00am`)
+	: console.log(`${raceNumber}
+The race will begin at 12:30pm`);
